050aa15051339559c3fa8ba7ffed3466cc6cc1c6
WeendyPalomo <58180852+WeendyPalomo@users.noreply.github.com>
2026-06-27 22:08:25 -0500

    Fix indentation and update print statements

--- ejercicio03.py
+++ ejercicio03.py
@@ -4,5 +4,6 @@ def confiabilidad_paralelo(probabilidades):
         Q *= (1.0 - p)  # Multiplica la probabilidad de falla de cada componente
     return 1.0 - Q
 
-componentes = [0.90, 0.85, 0.95]  # Coniabilidad de cada línea de red/componente
+# Datos de prueba 
+componentes = [0.90, 0.85, 0.95]  # Confiabilidad de cada línea de red/componente
 print(f"Confiabilidad del sistema paralelo: {confiabilidad_paralelo(componentes):.5f}")
